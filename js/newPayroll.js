5a1e00307a1e76dcefa9883be70d06fef3491a67
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-26 18:43:44 +0530

    [Mukul Dev] Add: Ability to create an on save object and give the alert notifivation

--- js/newPayroll.js
+++ js/newPayroll.js
@@ -22,3 +22,50 @@ window.addEventListener('DOMContentLoaded', (event) => {
         output.textContent=salary.value;
     });
 });
+
+//UC3
+const save=() =>{
+    try {
+        let employeePayrollData = createEmployeePayroll();
+    }catch (e) {
+        return;
+    }
+}
+const createEmployeePayroll = () =>{
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    } catch(e){
+        setTextvalue('.text-error',e);
+        throw e;
+    }
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let date = getInputValueById('#day') +" " + getInputValueById('#month')+" "+ getInputValueById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item =>{
+        if(item.checked) selItems.push(item.value);
+    });
+    return selItems;
+}
+
+//new method
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+//old method only for demo
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
