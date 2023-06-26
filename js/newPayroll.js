635889020c2b9aafda54be95f908f332233d1315
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-26 19:00:20 +0530

    [Mukul Dev] Add: We reset the employee payroll form by one click on reset button

--- js/newPayroll.js
+++ js/newPayroll.js
@@ -83,3 +83,30 @@ function createAndUpdateStoarge(employeePayrollData) {
     alert(employeePayrollList.toString());
     localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollData))
 }
+
+//UC5
+const resetForm =()=>{
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2023');
+}
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked =false;
+    });
+}
+const setTextvalue=(id,value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
